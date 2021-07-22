30d4863493cc50b3649020da251f20847e81c3ff
temro0v0 <merbina@naver.com>
2021-07-22 15:16:24 +0900

    스크롤트리거 연습

--- docs/pf/work1/index.js
+++ docs/pf/work1/index.js
@@ -1,3 +1,24 @@
 $('body').niceScroll({
   scrollspeed: 10
 });
+
+gsap.to('.txt_1_1', {
+  y:100,
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.txt_1_1',
+    markers:true,
+  },
+});
+
+gsap.to('.txt_1_2', {
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.txt_1_2',
+    markers:true,
+    start:'50% 100%',
+    end:'100% 0',
+  },
+});
